c951fe0c0315e4a44af5d68f2186f9f0c8787e6d
Spas Kalinov <spaskicha@gmail.com>
2018-11-03 20:52:47 +0200

    Player movement

---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+    //Config
+    [SerializeField]
+    float speed =  5f;
+
+    //State
+    bool isAlive = true;
+
+    //Cached component references
+    Rigidbody2D rb;
+    Animator animator;
+
+    //Message then methods
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+        FlipSprite();
+	}
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // -1/1
+        Vector2 velocity = new Vector2(controlThrow * speed, rb.velocity.y);
+        rb.velocity = velocity;
+
+        bool isMovingHorizontally = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", isMovingHorizontally);
+    }
+
+    private void FlipSprite()
+    {
+        bool isMovingHorizontally = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (isMovingHorizontally)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1);
+        }
+
+    }
+}
