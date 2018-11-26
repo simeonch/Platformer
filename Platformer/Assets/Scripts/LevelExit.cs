cc5f2446fade0efd9dc9c61669e3e6ba7e24d44a
Spas Kalinov <spaskicha@gmail.com>
2018-11-26 20:16:40 +0200

    Implemented level flow

---
+++ Platformer/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    [SerializeField]
+    float levelLoadDelay = 2f;
+
+    [SerializeField]
+    float slowMotionFactor = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = slowMotionFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
